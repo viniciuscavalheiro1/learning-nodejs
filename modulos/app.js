93c0ff74ad8bfd08bcf043b377e772d3ee57392f
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-04-16 00:09:26 -0300

    modulos

---
+++ modulos/app.js
@@ -0,0 +1,5 @@
+var calc = require('./calc');
+
+
+console.log(calc.soma(23, 27));
+console.log(calc.multi(23, 27));
---
+++ modulos/calc.js
@@ -0,0 +1,12 @@
+function soma(a, b) {
+  return a + b;
+}
+
+function multi(a, b) {
+  return a * b;
+}
+
+module.exports = {
+  soma,
+  multi,
+}
